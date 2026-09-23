2e728613901fb2e0c672aada1e73bc0b5ff4db42
Hyeopgeon Lee <soft1318@naver.com>
2026-09-23 11:03:20 +0900

    Configure production Google OAuth client

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.ALUMNI_CONFIG = Object.freeze({
   API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE',
-  GOOGLE_CLIENT_ID: 'PASTE_YOUR_GOOGLE_OAUTH_CLIENT_ID_HERE',
+  GOOGLE_CLIENT_ID: '986927581593-d1ilnuvgap3ibmp5rl2efe0ju4lgagkj.apps.googleusercontent.com',
   SITE_URL: 'https://alumni.k-bigdata.kr',
   REQUEST_TIMEOUT_MS: 15000
 });
