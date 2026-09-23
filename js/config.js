af698a3cdd3013b78ad0c5528d59355c90596211
Hyeopgeon Lee <soft1318@naver.com>
2026-09-23 11:23:06 +0900

    Connect frontend to production Apps Script API

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.ALUMNI_CONFIG = Object.freeze({
-  API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwMMOwofSybWFy3BYmPi9ol_VQsi_WFfjnEzvnrxUF4ncDZ5TQeS8tcRyOXcgCbgVU4/exec',
   GOOGLE_CLIENT_ID: '986927581593-d1ilnuvgap3ibmp5rl2efe0ju4lgagkj.apps.googleusercontent.com',
   SITE_URL: 'https://alumni.k-bigdata.kr',
   REQUEST_TIMEOUT_MS: 15000
